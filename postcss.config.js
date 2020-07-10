43cfe5af9ed4d0cea85f31bf6c8a106b9d8e92d1
zhangqian <zhangqian@szy.cn>
2020-07-10 18:06:40 +0800

    修改postcss的配置

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,15 @@ const autoprefixer = require('autoprefixer')
 module.exports = {
   plugins: [
     autoprefixer({
-      browsers: ['last 5 version']
+      overrideBrowserslist: [
+        "Android 4.1",
+        "iOS 7.1",
+        "Chrome > 31",
+        "ff > 31",
+        "ie >= 8"
+        //'last 10 versions', // 所有主流浏览器最近10版本用
+      ],
+      grid: true
     })
   ]
 }
